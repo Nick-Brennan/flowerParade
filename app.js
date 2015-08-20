6d41528d291d161ad15ebc783ee6fb31ab62fc9d
Nick Brennan <nick_p_b@yahoo.com>
2015-08-20 16:53:06 -0700

    added comments to index.html and app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//This is the short-hand form of the jquery document.ready()
 $(function(){
+
+	  //this line takes the script template(#flower-template) from the inline HTML script and creates a function using underscore.js
     var flowerTemplate = _.template($('#flower-template').html());
+
+     //this line uses the generated function to produce HTML dynamically.
     var flowerHtml = flowerTemplate(flower);
-    console.log(flower.color);
-    console.log(flowerHtml);
+    
+    //this line appends the dynamically generated HTML to a <div> with the id "container"
     $("#container").append(flowerHtml);
 });
